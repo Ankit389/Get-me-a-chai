b6655b67b8433218017ec6147d9b3b3066ed0008
Mohitsen11 <mohitsen0102@gmail.com>
2024-04-28 22:40:38 +0530

    fix: Added use-client where sessionProvider is used + correction in typos

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
       <nav className='bg-blue-900 text-white
        flex justify-between items-center px-4 h-16'>
         <div className='logo font-bold text-lg  flex   gap-2 justify-center items-center  '>
-         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr5l0yo86ZS5bt1XyKnykPX47oe5Z2RVKzPFanZuDFIg&s"  width={44}alt=""/>
+         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr5l0yo86ZS5bt1XyKnykPX47oe5Z2RVKzPFanZuDFIg&s"  width={44}alt="image"/>
          <span> GetmeChai</span>
         
 
@@ -33,7 +33,7 @@ const Navbar = () => {
 
 <div>
    <Link href={"/login"}>
-<button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Login</button></Link>
+<button type="button" className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Login</button></Link>
 </div>
       </nav>
   )
